0bc5330c5758860de367466a805ac3ad38ae0022
Sinan <xiesinan3@gmail.com>
2017-02-16 16:51:00 -0500

    First MVC Practice

---
+++ CSharp_MVC/FirstMVCApplication/FirstMVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVCApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+
+            //  MVC uses the ViewBag object to pass data between Controller and View.
+            ViewBag.Greeting =
+                hour < 12 ? "Good Moring. Time is " + DateTime.Now.ToShortDateString()
+                          : "Good Afternoon. Time is " + DateTime.Now.ToShortDateString();
+            return View();
+        }
+
+    }
+}
+ 
